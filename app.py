b2306cad59ce5f8ee20ee428dad92e522fa23b95
Tahabpoker <pokertahab@gmail.com>
2024-05-25 00:59:56 +0530

    nothing

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 # app.py
-
-import pandas as pd
-from flask import Flask, render_template, request
+import pandas as pd #type: ignore
+from flask import Flask, render_template, request #type: ignore
 
 # Read the Excel file into a data frame
-df = pd.read_excel('excel.xlsx')
+df = pd.read_excel('./static/voters_list.xlsx')
 
 # Flask constructor
 app = Flask(__name__)
@@ -16,7 +15,7 @@ def index():
         data_name = request.form.get('data_name')
         data_column = request.form.get('data_column')
 
-        if data_name and (data_column == "Bar" or data_column == "Foo" ) :
+        if data_name and (data_column ==  "Electoral Name" or "Relative Name" or "Epic No") :
             # Convert search value and column values to lowercase
             data_name_lower = data_name.lower()
             df_column_lower = df[data_column].str.lower()
@@ -29,24 +28,31 @@ def index():
                 first_row_data = apple_rows.iloc[0]
 
                 # Access individual cell values using column names
-                cell1_value = first_row_data['Bar']
-                cell2_value = first_row_data['text']
-                cell3_value = first_row_data['Foo']
+                cell1_value = first_row_data['Sr No']#
+                cell2_value = first_row_data['List No']#
+                cell3_value = first_row_data['Electoral Name']#
+                cell4_value = first_row_data['Relative Name']#
+                cell5_value = first_row_data['Address']#
+                cell6_value = first_row_data['School']#
+                cell7_value = first_row_data['Age']#
+                cell8_value = first_row_data['Gender']#
+                cell9_value = first_row_data['Epic No']#
+                
 
                 # Render the HTML template with the cell values
-                return render_template('index.html', cell1_value=cell1_value, cell2_value=cell2_value, cell3_value=cell3_value)
+                return render_template('index.html', cell1_value=cell1_value, cell2_value=cell2_value, cell3_value=cell3_value, cell4_value=cell4_value, cell5_value=cell5_value, cell6_value=cell6_value, cell7_value=cell7_value, cell8_value=cell8_value, cell9_value=cell9_value)
             else:
                 error_message = "Value '{}' not found in the specified column '{}'".format(data_name, data_column)
                 return render_template('index.html', error_message=error_message)
             
-        elif data_name and data_column == "text":
+        elif data_name and (data_column == "Sr No" or "List No" or "Gender" or "Age" or "Address" or "School"):
             data_name_lower = data_name.lower()
             df_column_lower = df[data_column].str.lower()
 
             apple_rows = df[df_column_lower == data_name_lower]
+            # apple_rows = pd.read_excel("voter_list_data_1.xlsx")
             # rows_to_html = apple_rows.to_html()
-
-            return render_template('print_excel_table.html',  tables=[apple_rows.to_html(classes='data', header="true")])
+            return render_template('print_excel_table.html',  tables =[apple_rows.to_html(classes='data', header="true")])
 
         else:
             error_message = "Please provide both a value and select a column."
--- test.py
+++ test.py
@@ -1,26 +1,71 @@
 import pandas as pd # type: ignore 
 
+head =[ "Sr No",	"List No",	"Electoral Name,"	"Relative Name",	"Address" ," School",	"Age",	"Gender",	"Epic No", "Is Photo"]
 # Read the Excel file into a data frame
-df = pd.read_excel('excel.xlsx')
+df = pd.read_excel('./static/voters_list.xlsx', names=head)
 
-data_name = "England"
+data_name = "1"
+data_column = "Sr No"
 
-# Search for the value in the first column (assuming "Bar" is the first column)
-apple_rows = df[df['Bar'] == data_name]
+data_name_lower = data_name.lower()
+df_column_lower = df[data_column].astype(str).lower()
 
-if not apple_rows.empty:
-    # Extract the first row as a Series (single row)
-    first_row_data = apple_rows.iloc[0]
+df[data_name_lower] = df[data_name_lower].astype(str)   
 
-    # Access individual cell values using column names
-    cell1_value = first_row_data['Bar']  # Access by column name 'Bar'
-    cell2_value = first_row_data['text']  # Assuming 'Food' is the second column name
-    cell3_value = first_row_data['Foo']  # Assuming 'Third_Column' is the third column name
+apple_rows = df[data_column == data_name_lower]
 
-    # Display results
-    print("Cell 1 value:", cell1_value)
-    print("Cell 2 value:", cell2_value)
-    print("Cell 3 value:", cell3_value)
 
-else:
-    print("Value '{}' not found in the first column.".format(data_name))
+
+
+print(apple_rows.to_html())
+# print(df)
+
+
+
+# data_name = "2"
+# data_column = "1"
+
+# data_name_lower = data_name.lower()
+# df_column_lower = df[data_column].str.lower()
+
+# apple_rows = df[df_column_lower == data_name_lower]
+
+# print(apple_rows)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# data_name = "England"
+
+# # Search for the value in the first column (assuming "Bar" is the first column)
+# apple_rows = df[1 == data_name]
+
+# if not apple_rows.empty:
+#     # Extract the first row as a Series (single row)
+#     first_row_data = apple_rows.iloc[0]
+
+#     # Access individual cell values using column names
+#     cell1_value = first_row_data['Bar']  # Access by column name 'Bar'
+#     cell2_value = first_row_data['text']  # Assuming 'Food' is the second column name
+#     cell3_value = first_row_data['Foo']  # Assuming 'Third_Column' is the third column name
+
+#     # Display results
+#     print("Cell 1 value:", cell1_value)
+#     print("Cell 2 value:", cell2_value)
+#     print("Cell 3 value:", cell3_value)
+
+# else:
+#     print("Value '{}' not found in the first column.".format(data_name))
