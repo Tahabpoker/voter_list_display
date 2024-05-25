47358a2ffadd7da7cbeccff5de6298faf6f5ac6a
Tahabpoker <pokertahab@gmail.com>
2024-05-26 04:34:11 +0530

    dd

--- app.py
+++ app.py
@@ -153,7 +153,8 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
+    
 
 
 # @app.route('/', methods=['GET', 'POST'])
