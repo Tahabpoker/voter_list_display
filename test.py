f34c6a9708d0c6e741b992cb26f7d5f92762c8ae
Maaz Khan <maaz.khan25925@gmail.com>
2024-05-26 04:05:08 +0530

    feat: adjust style

--- test.py
+++ test.py
@@ -4,28 +4,35 @@ df_152 = pd.read_excel("./static/152.xlsx")
 
 vidhan_Sabha = "152"
 
-data_name = "jijiji"
+data_name = "XWC1569359"
 data_column = "Epic No"
 
 
 def search(data_name, data_column, df):
     data_name_lower = data_name.lower()
-    df_column_lower = df[data_column].str.lower()
+    df_column_lower = df[data_column]
+    print(df_column_lower)
 
     apple_rows = df[df_column_lower == data_name_lower]
     return apple_rows
 
 
-match vidhan_Sabha:
-    case "152": 
-        tables = search(data_name, data_column, df_152)
-        if not len(tables):
-            print("empty")
-        else:
-            print("full")
+# list_numbers = [str(i) for i in range(152, 188)]
+# if vidhan_Sabha in list_numbers:
+#     print(search(data_name, data_column, "df_{}.".format(vidhan_Sabha)))
 
-data_columns = ["Epic No", "Sr No", "School"]
+# match vidhan_Sabha:
+#     case "152": 
+#         tables = search(data_name, data_column, df_152)
+#         if not len(tables):
+#             print("empty")
+#         else:
+#             print("full")
 
+# data_columns = ["Epic No", "Sr No", "School"]
+
+# print(df_152[data_column].str.lower() == data_name.lower())
+print(search(data_name, data_column, df_152))
 
 '''
 '''
